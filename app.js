76219a0450eb5034b1b4e1dccca8e9637f01a3d3
Brad Dunbar <dunbarb2@gmail.com>
2015-10-10 07:40:49 -0400

    Assets!

--- app.js
+++ app.js
@@ -20,7 +20,6 @@ app.use(session({
 }))
 app.use(body.urlencoded({extended: false}))
 app.use(multer({dest: './tmp/uploads/', putSingleFilesInArray: true}))
-app.use(ozymandias.static('public'))
 app.use(require('./mid/user'))
 app.use(require('./mid/flash'))
 
