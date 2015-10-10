c87a714a6933d0165a573c28302ea2002698d775
Brad Dunbar <dunbarb2@gmail.com>
2015-10-10 07:53:31 -0400

    Asset paths.

--- helpers/index.js
+++ helpers/index.js
@@ -8,3 +8,10 @@ exports.mapsKey = 'AIzaSyAzuFFZQFL8uQCUS2Dx9MJYDq1t0WOD3js'
 
 // Medfusion portal url
 exports.portalUrl = 'https://www.medfusion.net/secure/portal/index.cfm?fuseaction=home.login&dest=welcome&gid=4442&muuid=4727'
+
+// Assets
+
+let assetPath = exports.assetPath = function (path) {
+  if (process.env.NODE_ENV !== 'production') return `/${path}`
+  return require('../assets.json')[path]
+}
