fdf1d758c0a06c12ba48c032d39212d298e2f522
Brad Dunbar <dunbarb2@gmail.com>
2015-12-16 08:12:00 -0500

    Show/Delete photos.

--- js/app.js
+++ js/app.js
@@ -5,3 +5,8 @@ var $message = $('#message')
 setTimeout(function () {
   $message.removeClass('active')
 }, 10000)
+
+// Handle submit confirmations.
+$(document).on('click', '[data-confirm]', function (e) {
+  if (!confirm($(e.target).data('confirm'))) e.preventDefault()
+})
--- routes/admin/photos.js
+++ routes/admin/photos.js
@@ -6,17 +6,32 @@ const upload = require('multer')({dest: 'tmp/uploads'})
 
 router.find('photo', () => db.Photo)
 
+// Index
 router.get('/', (req, res) => {
   db.Photo.not({image_updated_at: null}).all().then((photos) => {
     res.render('admin/photos/index', {photos: photos})
   }).catch(res.error)
 })
 
+// Show
+router.get('/:photo_id', (req, res) => {
+  res.render('admin/photos/show', {photo: req.photo})
+})
+
+// Upload
 router.post('/', upload.single('image'), (req, res) => {
   db.Photo.create({}).then((photo) => {
     return photo.uploadImage(req.file).then(() => {
       res.flash('Image Uploaded.')
       res.redirect(req.body.return_to)
     })
+  }).catch(res.error)
+})
+
+// Delete
+router.post('/:photo_id/delete', (req, res) => {
+  req.photo.destroy().then(() => {
+    res.flash('success', 'Photo Deleted')
+    res.redirect('/admin/photos')
   }).catch(res.error)
 })
