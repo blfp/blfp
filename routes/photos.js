60c5d1846df6e317253e5552f8ed1e4a7f950c3f
Brad Dunbar <dunbarb2@gmail.com>
2015-12-16 10:11:51 -0500

    No photos message, fix the paging.

--- routes/photos.js
+++ routes/photos.js
@@ -5,7 +5,7 @@ const router = module.exports = require('ozymandias').Router()
 
 router.get('/', function (req, res) {
   const page = res.locals.page = +(req.query.page || 1)
-  db.Photo.not({image_updated_at: null}).paginate(page, 4).then((photos) => {
+  db.Photo.not({image_updated_at: null}).paginate(page, 25).then((photos) => {
     res.render('photos/index', {photos: photos})
   }).catch(res.error)
 })
